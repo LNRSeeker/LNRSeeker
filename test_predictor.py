f356c651d18e664be47b133cdecc49795076ae8c
xingyuwei <aczzdx@gmail.com>
2017-08-28 16:44:48 +0800

    adding testing codes

---
+++ test_predictor.py
@@ -0,0 +1,18 @@
+import logging
+
+from predictor import predictor
+
+logging.basicConfig(filename="something.log")
+logger = logging.getLogger("temp")
+
+if __name__ == '__main__':
+    LNCRNA_TRAIN = "/Users/xingyuwei/NOTE/M1/lncrna_train_mini.fa"
+    GENE_TRAIN = "/Users/xingyuwei/NOTE/M1/gene_train_mini.fa"
+
+    model = predictor()
+    model.logger = logger
+    model.train(GENE_TRAIN, LNCRNA_TRAIN)
+
+    import pickle as pkl
+    with open("mymodel.pkl", "rb") as f:
+        pkl.dump(model, f)
